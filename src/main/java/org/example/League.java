ae89e686c94547d39b47b9b08f75341ae3b65306
AKASH ANAND <68923458+AKASHANAND8029@users.noreply.github.com>
2022-02-15 15:01:47 +0530

    SoccerLeagueSpringMVC

---
+++ src/main/java/org/example/League.java
@@ -0,0 +1,20 @@
+package org.example;
+
+
+import lombok.*;
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Setter
+    @Getter
+    @ToString
+    public class League {
+
+        private String leagueName;
+        private Integer leagueYear;
+        private String leagueSeason;
+
+
+
+
+    }
---
+++ src/main/java/org/example/controller/LeagueController.java
@@ -0,0 +1,27 @@
+package org.example.controller;
+import org.example.League;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/league")
+public class LeagueController {
+    @GetMapping("/showForm")
+    public String showForm()
+    {
+        return "show-form";
+    }
+    @PostMapping("/processForm")
+    public String processForm(@RequestParam("lName") String name, @RequestParam("lYear") Integer year, @RequestParam("lSeason") String season, Model theModel)
+    {
+        League league=new League(name,year,season);
+
+        theModel.addAttribute("LEAGUE",league);
+
+        return "process-form";
+    }
+}
