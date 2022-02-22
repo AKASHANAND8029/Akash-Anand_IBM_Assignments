cca56585f94309e99aad1a891c273fa2ddfa0a48
AKASH ANAND <68923458+AKASHANAND8029@users.noreply.github.com>
2022-02-22 11:08:29 +0530

    Add files via upload

---
+++ ThymeLeafEmployeeManagemant/src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,59 @@
+package com.example.demo.model;
+
+import java.util.Random;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+	@Id
+	@Column(name = "id")
+	private Integer id=new Random().nextInt(1000);
+	@Column(name ="first_name")
+	private String firstName;
+	@Column(name ="last_name")
+	private String lastName;
+	@Column(name ="email")
+	private String email;
+	
+
+public Employee() {
+	
+}
+public Employee(int id, String firstName, String lastName, String email) {
+	
+	this.id = id;
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.email = email;
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getFirstName() {
+	return firstName;
+}
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+public String getLastName() {
+	return lastName;
+}
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+
+}
